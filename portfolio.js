616757c7e827756549ca7a65bdd35d275f77da3a
cymero <cymero230.email.com>
2025-06-07 02:38:42 -0500

    initial clean push

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,15 @@
-function toggleMenu() {
-    document.querySelector(".links").classList.toggle("active");
-  };
+const menuToggle = document.querySelector(".menu-toggle");
+const links = document.querySelector(".links");
+const navLinks = document.querySelectorAll(".links a");
+
+menuToggle.addEventListener("click", ()=> {
+  links.classList.toggle("open");
+  menuToggle.classList.toggle("open");
+});
+
+navLinks.forEach(link => {
+  link.addEventListener("click", ()=> {
+    links.classList.remove("open");
+    menuToggle.classList.remove("open");
+  });
+});
